adcf33c0b34acb13ee5c3f532e9fa53406024fde
anafuddin <anaf.uddin@gmail.com>
2023-11-10 14:33:20 -0600

    fixed documents bg

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -26,7 +26,7 @@ const MainLayout = ({
     }
     
     return (
-        <div className="flex dark:bg-[#1F1F1F]">
+        <div className="h-full flex dark:bg-[#1F1F1F]">
           <Navigation />
           <main className="flex-1 h-full overflow-y-auto">
             <SearchCommand />
